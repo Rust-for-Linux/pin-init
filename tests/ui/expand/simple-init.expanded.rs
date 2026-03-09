0d5cd7f5689b2a622752ab8a88cb84ecbf5b69f1
Benno Lossin <lossin@kernel.org>
2026-03-09 09:13:46 +0100

    replace shadowed return token by `unsafe`-to-create token
    We use a unit struct `__InitOk` in the closure generated by the
    initializer macros as the return value. We shadow it by creating a
    struct with the same name again inside of the closure, preventing early
    returns of `Ok` in the initializer (before all fields have been
    initialized).
    
    In the face of Type Alias Impl Trait (TAIT) and the next trait solver,
    this solution no longer works [1]. The shadowed struct can be named
    through type inference. In addition, there is an RFC proposing to add
    the feature of path inference to Rust, which would similarly allow [2]
    
    Thus remove the shadowed token and replace it with an `unsafe` to create
    token.
    
    The reason we initially used the shadowing solution was because an
    alternative solution used a builder pattern. Gary writes [3]:
    
        In the early builder-pattern based InitOk, having a single InitOk
        type for token is unsound because one can launder an InitOk token
        used for one place to another initializer. I used a branded lifetime
        solution, and then you figured out that using a shadowed type would
        work better because nobody could construct it at all.
    
    The laundering issue does not apply to the approach we ended up with
    today.
    
    With this change, the example by Tim Chirananthavat in [1] no longer
    compiles and results in this error:
    
        error: cannot construct `pin_init::__internal::InitOk` with struct literal syntax due to private fields
          --> src/main.rs:26:17
           |
        26 |                 InferredType {}
           |                 ^^^^^^^^^^^^
           |
           = note: private field `0` that was not provided
        help: you might have meant to use the `new` associated function
           |
        26 -                 InferredType {}
        26 +                 InferredType::new()
           |
    
    Applying the suggestion of using the `::new()` function, results in
    another expected error:
    
        error[E0133]: call to unsafe function `pin_init::__internal::InitOk::new` is unsafe and requires unsafe block
          --> src/main.rs:26:17
           |
        26 |                 InferredType::new()
           |                 ^^^^^^^^^^^^^^^^^^^ call to unsafe function
           |
           = note: consult the function's documentation for information on how to avoid undefined behavior
    
    Reported-by: Tim Chirananthavat <theemathas@gmail.com>
    Link: https://github.com/rust-lang/rust/issues/153535 [1]
    Link: https://github.com/rust-lang/rfcs/pull/3444#issuecomment-4016145373 [2]
    Link: https://github.com/rust-lang/rust/issues/153535#issuecomment-4017620804 [3]
    Signed-off-by: Benno Lossin <lossin@kernel.org>

--- tests/ui/expand/simple-init.expanded.rs
+++ tests/ui/expand/simple-init.expanded.rs
@@ -2,24 +2,19 @@ use pin_init::*;
 struct Foo {}
 fn main() {
     let _ = {
-        struct __InitOk;
         let __data = unsafe {
             use ::pin_init::__internal::HasInitData;
             Foo::__init_data()
         };
         let init = ::pin_init::__internal::InitData::make_closure::<
             _,
-            __InitOk,
             ::core::convert::Infallible,
         >(
             __data,
             move |slot| {
-                {
-                    struct __InitOk;
-                    #[allow(unreachable_code, clippy::diverging_sub_expression)]
-                    let _ = || unsafe { ::core::ptr::write(slot, Foo {}) };
-                }
-                Ok(__InitOk)
+                #[allow(unreachable_code, clippy::diverging_sub_expression)]
+                let _ = || unsafe { ::core::ptr::write(slot, Foo {}) };
+                Ok(unsafe { ::pin_init::__internal::InitOk::new() })
             },
         );
         let init = move |
