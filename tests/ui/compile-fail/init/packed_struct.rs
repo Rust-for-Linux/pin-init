1f5ec9cc149fa2465923c7e2abcb5534890c1916
Gary Guo <gary@garyguo.net>
2026-04-23 14:47:14 +0100

    internal: move alignment check to `make_field_check`
    Instead of having the reference creation serving dual-purpose as both for
    let bindings and alignment check, detangle them so that the alignment check
    is done explicitly in `make_field_check`. This is more robust again
    refactors that may change the way let bindings are created.
    
    Reviewed-by: Alice Ryhl <aliceryhl@google.com>
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- tests/ui/compile-fail/init/packed_struct.rs
+++ tests/ui/compile-fail/init/packed_struct.rs
@@ -1,6 +1,7 @@
 use pin_init::*;
 
 #[repr(C, packed)]
+#[derive(Zeroable)]
 struct Foo {
     a: i8,
     b: i32,
@@ -8,4 +9,8 @@ struct Foo {
 
 fn main() {
     let _ = init!(Foo { a: -42, b: 42 });
+    let _ = init!(Foo {
+        b: 42,
+        ..Zeroable::init_zeroed()
+    });
 }
